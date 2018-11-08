e11361d1f5a6b0b38c0636ff08309a76b2d55ce3
Radthiek <44319561+Radthiek@users.noreply.github.com>
2018-11-08 21:10:23 +0100

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off 
+:: Script created by TheRacingLion (https://github.com/TheRacingLion) [ 6 / 12 / 2016 ] 
+:: Bot created by Alipoodle (https://github.com/Alipoodle) [ 28 / 09 / 2017 ] 
+:: -*Read LICENSE to know more about permissions*- 
+title Discord ShadeBot 
+:START 
+node app.js 
+goto START 
